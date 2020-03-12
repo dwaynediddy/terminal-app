fc6dd933fef69defbf47b7fccba9bf0adfe7ee70
dwaynediddy <burrows.dwayne@icloud.com>
2020-03-12 14:59:44 +1000

    added ruby gem rspec

--- src/customer/customer_list.rb
+++ src/customer/customer_list.rb
@@ -1,21 +1,21 @@
 def customer_list 
     return [{
 
-    customer: "Big Poppa",
+    customer: "Big Poppa".colorize(:blue),
     price: 40,
     done: "for sure ill take that!",
     fail: "you think im made of money?! no way"
 
     }, 
     {
-    customer: "Swaggy P",
+    customer: "Swaggy P".colorize(:red),
     price: 70,
     done: "helluva deal ill take those!",
     fail: "you must be outside your mind!"
 
     },
     {
-    customer: "Jezenizzle fo shizzle",
+    customer: "Jezenizzle fo shizzle".colorize(:yellow),
     price: 90,
     done: "yeah ill take those i got that full-stack dev money",
     fail: "you think i fell off the turnip truck yesterday!"
